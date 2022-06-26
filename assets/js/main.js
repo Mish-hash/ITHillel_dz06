d295f1614f540e9d5bcb591b9ad743fa8c6f00b8
mish-hash <poluektov23@gmail.com>
2022-06-26 20:08:38 +0300

    2. Created function filterArray

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,24 @@ function fillArray(count, data){
 const fillArray2 = (count, data) => [...Array(count)].map( el => data );
 
 console.log(fillArray(10, 'qwerty'), fillArray2(10, 'qwerty'));
+
+
+/*
+2) Напишите функцию filterArray, которая очищает массив от нежелательных значений (false, undefined, '', 0, null), СПИСОК КОТОРЫХ ПЕРЕДАН ВВИДЕ АРГУМЕНТОВ ФУНКЦИИ.
+
+let array = [0, 1, 2, null, undefined, 'qwerty', false];
+let result = filterArray(array, false, undefined, '', 0, null);
+// result - [1,2, 'qwerty'];
+*/
+
+let array = [0, 1, 2, null, undefined, 'qwerty', false];
+
+function filterArray(array, ...args) {
+    args.forEach(argument => array = array.filter(el => el !== argument));
+    return array;
+}
+
+const result = filterArray(array, false, undefined, '', 0, null);
+
+console.log(result);
+
